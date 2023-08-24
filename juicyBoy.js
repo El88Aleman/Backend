533050f73956104d07748397ee54b34058e8d7b1
El88Aleman <113607297+El88Aleman@users.noreply.github.com>
2023-08-24 12:31:38 -0300

    primer commit

--- juicyBoy.js
+++ juicyBoy.js
@@ -1,88 +1,56 @@
+const fs = require("fs");
+
 class ProductManager {
-  constructor() {
-    this.products = [];
-    this.productIdCounter = 1;
+  constructor(path) {
+    this.path = path;
   }
 
-  addProduct(productData) {
-    // Validar que todos los campos obligatorios estén presentes
-    if (
-      !productData.title ||
-      !productData.description ||
-      !productData.price ||
-      !productData.thumbnail ||
-      !productData.code ||
-      !productData.stock
-    ) {
-      console.log("Error: Todos los campos son obligatorios.");
-      return;
-    }
-
-    // Validar que el campo "code" no se repita
-    const codeExists = this.products.some(
-      (product) => product.code === productData.code
-    );
-    if (codeExists) {
-      console.log("Error: El código ya existe.");
-      return;
-    }
-
+  addProduct(product) {
+    const products = this.getProducts();
     const newProduct = {
-      id: this.productIdCounter,
-      title: productData.title,
-      description: productData.description,
-      price: productData.price,
-      thumbnail: productData.thumbnail,
-      code: productData.code,
-      stock: productData.stock,
+      id: products.length > 0 ? products[products.length - 1].id + 1 : 1,
+      ...product,
     };
-
-    this.products.push(newProduct);
-    this.productIdCounter++;
-
-    console.log("Producto agregado con éxito.");
+    products.push(newProduct);
+    this.saveProducts(products);
   }
 
   getProducts() {
-    return this.products;
+    try {
+      const data = fs.readFileSync(this.path, "utf-8");
+      return JSON.parse(data);
+    } catch (error) {
+      return [];
+    }
   }
 
   getProductById(id) {
-    const product = this.products.find((product) => product.id === id);
-    if (product) {
-      return product;
+    const products = this.getProducts();
+    return products.find((product) => product.id === id);
+  }
+
+  updateProduct(id, updatedFields) {
+    const products = this.getProducts();
+    const index = products.findIndex((product) => product.id === id);
+
+    if (index !== -1) {
+      products[index] = { ...products[index], ...updatedFields };
+      this.saveProducts(products);
+      return true;
     } else {
-      console.log("Error: Producto no encontrado.");
+      return false;
     }
+  }
+
+  deleteProduct(id) {
+    const products = this.getProducts();
+    const updatedProducts = products.filter((product) => product.id !== id);
+    this.saveProducts(updatedProducts);
+  }
+
+  saveProducts(products) {
+    fs.writeFileSync(this.path, JSON.stringify(products, null, 2), "utf-8");
   }
 }
 
-// Ejemplo de uso
-const productManager = new ProductManager();
-
-productManager.addProduct({
-  title: "Remera Negra",
-  description: "Oversize",
-  price: 8900,
-  thumbnail:
-    "https://res.cloudinary.com/dfcnmxndf/image/upload/v1689166163/zf25zvunabspnelqj7bj.jpg",
-  code: "REME01",
-  stock: 10,
-});
-
-productManager.addProduct({
-  title: "Remera Blanca",
-  description: "Oversize",
-  price: 8900,
-  thumbnail:
-    "https://res.cloudinary.com/dfcnmxndf/image/upload/v1689167338/hr38owfqdmugljsm9vet.jpg",
-  code: "REME02",
-  stock: 5,
-});
-
-console.log(productManager.getProducts());
-
-const foundProduct = productManager.getProductById(1);
-console.log(foundProduct);
-
-const notFoundProduct = productManager.getProductById(3);
+module.exports = ProductManager;
